6072560cb4760fce4fa84fc27ccc6e85607b4d01
magicpro97 <linhnt99x@gmail.com>
2019-09-16 09:13:36 +0700

    Refactor code

--- lib/features/detail/ui/widget/note_tarot_button.dart
+++ lib/features/detail/ui/widget/note_tarot_button.dart
@@ -10,59 +10,72 @@ class NoteTarotButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final titleStyle = Theme.of(context).textTheme.title;
-    final textFocusNote = FocusNode();
     final detailBloc = GetIt.I<DetailBloc>();
 
     return BlocBuilder(
       bloc: detailBloc,
-      builder: (context, state) => FloatingActionButton(
-        child: StreamBuilder<bool>(
-            stream: detailBloc.noteStateStream,
-            builder: (context, snapshot) {
-              if (!snapshot.hasData) return Icon(Icons.note);
+      builder: (context, state) =>
+          StreamBuilder<bool>(
+              stream: detailBloc.noteStateStream,
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) return Container();
+                return FloatingActionButton(
+                  child: snapshot.data ? Icon(Icons.save) : Icon(Icons.note),
+                  tooltip: snapshot.data ? "Save" : "Open",
+                  onPressed: () {
+                    if (detailBloc.isNoteOpen) {
+                      detailBloc.dispatch(CloseNote());
+                    } else {
+                      final bottomSheetController = showBottomSheet(
+                          context: context, builder: (context) => NoteSheet());
+                      detailBloc.dispatch(OpenNote(bottomSheetController));
+                    }
+                  },
+                );
+              }),
+    );
+  }
+}
 
-              return snapshot.data ? Icon(Icons.save) : Icon(Icons.note);
-            }),
-        onPressed: () {
-          if (detailBloc.isNoteOpen) {
-            detailBloc.dispatch(CloseNote());
-          } else {
-            final bottomSheetController = showBottomSheet(
-                context: context,
-                builder: (context) => Container(
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius:
-                            BorderRadius.vertical(top: Radius.circular(32.0)),
-                      ),
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 8.0, right: 8.0, top: 24.0, bottom: 8.0),
-                            child: Text(
-                              "Say your thinking about the card:",
-                              style: titleStyle,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: TextFormField(
-                              decoration: InputDecoration(
-                                  hintText: "Type something here..."),
-                              focusNode: textFocusNote,
-                              minLines: 3,
-                              maxLines: 6,
-                            ),
-                          )
-                        ],
-                      ),
-                    ));
-            detailBloc.dispatch(OpenNote(bottomSheetController));
-          }
-        },
+class NoteSheet extends StatelessWidget {
+  const NoteSheet({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final titleStyle = Theme
+        .of(context)
+        .textTheme
+        .title;
+    final textFocusNote = FocusNode();
+
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.vertical(top: Radius.circular(32.0)),
+      ),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 8.0, right: 8.0, top: 24.0, bottom: 8.0),
+            child: Text(
+              "Say your thinking about the card:",
+              style: titleStyle,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextFormField(
+              decoration: InputDecoration(hintText: "Type something here..."),
+              focusNode: textFocusNote,
+              minLines: 3,
+              maxLines: 6,
+            ),
+          )
+        ],
       ),
     );
   }
